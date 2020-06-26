9b9d6bb381759040ae135b487888f6e82ce8092a
wildenali <wildenwildenaliali@gmail.com>
2020-06-27 00:00:27 +0700

    CRUD dengan MySQL

--- 013-MySQL_CRUD.js
+++ 013-MySQL_CRUD.js
@@ -1,8 +1,10 @@
 var http = require('http');
 var url = require('url');
+var qString = require('querystring');
 var router = require('routes')();
 var view = require('swig');
 var mysql = require('mysql');
+
 var connection = mysql.createConnection({
     host : "localhost",
     port : 3306,
@@ -16,53 +18,94 @@ const port = 4000;
 router.addRoute('/', function (req,res) {
     connection.query("select * from mahasiswa", function (err, rows, field) {
         if (err) throw err;
-        console.log(rows);
-        rows.forEach(function(item) {
-            console.log("Nama: " + item.nama + " - NIM: " + item.nim);
+        
+        var html = view.compileFile('./013-index.html')({
+            title : "Data Mahasiswa",
+            data : rows
         });
-        // res.end("Coba mysql");
-
-        // nampilkan ke browser
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        res.end(JSON.stringify(rows));
-    })
+        res.writeHead(200, {"Content-Type" : "text/html"});
+        res.end(html);
+    });
 });
 
 router.addRoute('/insert', function (req,res) {
+    if (req.method.toUpperCase() == "POST") {
+        var dataPost = "";
+        req.on('data', function (chuncks) {
+            dataPost += chuncks;
+        });
 
-    connection.query("insert into mahasiswa set ? ", {
-        nim : 11710005,
-        nama : 'Gehu',
-        alamat : 'Papua'
-    },function(err, field){
-        if (err) throw err;
-        
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        res.end(field.affectedRows + " record(s) updated");
-    });
+        req.on('end', function () {
+            dataPost = qString.parse(dataPost);
+            console.log(dataPost);
+            
+            connection.query("insert into mahasiswa set ? ", dataPost,
+                function(err, field){
+                    if (err) throw err;
+                    
+                    res.writeHead(302, {"Location": "/"});
+                    res.end();
+                }
+            );
+        });
+    } else {
+        var html = view.compileFile('./013-form.html')();
+        res.writeHead(200, {"Content-Type" : "text/html"});
+        res.end(html);
+    }
 
 });
 
-router.addRoute('/update', function (req,res) {
-    connection.query("update mahasiswa set ? where ?",[
-        { nama : "Aliw" },
-        { nim : "11710001" }
-    ], function (err, fields) {
-        if(err) throw err;
+router.addRoute('/update/:id', function (req,res) {
+    connection.query("select * from mahasiswa where ?",
+        { nim : this.params.id },
+        function(err,rows,field) {
+            if (rows.length) {
+                var data = rows[0];
+                if (req.method.toUpperCase() == "POST") {
+                    var dataPost = "";
+                    req.on('data', function (chuncks) {
+                        dataPost += chuncks;
+                    });
 
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        res.end(fields.changedRows + " Rows Updated");
-    });
+                    req.on('end', function () {
+                        dataPost = qString.parse(dataPost);
+                        console.log(dataPost);
+                        connection.query("update mahasiswa set ? where ?",[
+                                dataPost,
+                                { nim : data.nim }
+                            ], function (err, fields) {
+                                if(err) throw err;
+
+                                res.writeHead(302, {"Location": "/"});
+                                res.end();
+                            }
+                        );
+                    })
+                } else {
+                    var html = view.compileFile('./013-form_update.html')({
+                        data : data
+                    });
+                    res.writeHead(200, {"Content-Type" : "text/html"});
+                    res.end(html);
+                }
+            } else {
+                var html = view.compileFile('./013-404.html')();
+                res.writeHead(404, {"Content-Type": "text/html"});
+                res.end(html);
+            }
+        }
+    );
 });
 
-router.addRoute('/delete', function (req,res) {
+router.addRoute('/delete/:id', function (req,res) {
     connection.query("delete from mahasiswa where ?",{
-        nim : "11710001" 
+        nim : this.params.id
     }, function (err, fields) {
         if(err) throw err;
 
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        res.end(fields.affectedRows + " Rows Delete");
+        res.writeHead(302, {"Location": "/"});
+        res.end();
     });
 });
 
