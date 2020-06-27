5dceff20beda2428fbe1364b4eae98fec595381c
wildenali <wildenwildenaliali@gmail.com>
2020-06-27 15:39:11 +0700

    setting routes di server.js

--- 101-restAPI/101-server.js
+++ 101-restAPI/101-server.js
@@ -8,6 +8,10 @@ const port = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// 105-routes, panggil routes
+var routes = require('./105-routes');
+routes(app);
+
 app.listen(port, () => {
     console.log(`Server started on port http://localhost:${port}`);
 });
