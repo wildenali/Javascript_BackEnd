c1771106c4d142a9118bb15ecf9fcb6b68f30089
wildenali <wildenwildenaliali@gmail.com>
2020-06-28 13:57:16 +0700

    berhasil membuat controller login

--- 101-restAPI/106-middleware/index.js
+++ 101-restAPI/106-middleware/index.js
@@ -7,4 +7,7 @@ var router = express.Router();
 // daftarkan menu registrasi
 router.post('/api/v1/registrasi', auth.registrasi);
 
+// daftarkan menu login
+router.post('/api/v1/login', auth.login);
+
 module.exports = router;
