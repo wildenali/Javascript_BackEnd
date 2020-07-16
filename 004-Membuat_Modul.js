b60c011091c7fc8d0ab9f96e14fa969b20d0a18b
wildenali <wildenwildenaliali@gmail.com>
2020-07-16 09:20:09 +0700

    membuat modul sendiri

---
+++ 004-Membuat_Modul.js
@@ -0,0 +1,9 @@
+const op = require("./004-Membuat_Modul_Kalkulator");
+
+console.log(op.title);
+
+const moduleTambah = op.tambah(16, 3);
+console.log(moduleTambah);
+
+const moduleKali = op.kali(16, 45);
+console.log(moduleKali);
