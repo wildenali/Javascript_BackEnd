5cb099a236f78c2f7ab0ccf003689c27d890b68e
wildenali <wildenwildenaliali@gmail.com>
2020-07-16 14:24:25 +0700

    how to read database

---
+++ monggoDB_basic/002-ReadData.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/db-mongoose', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const fruitSchema = new mongoose.Schema({
+    name: String,
+    rating: Number,
+    review: String
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+Fruit.find(function(error, fruits) {
+    if (error) {
+        console.log(error);
+    } else {
+        mongoose.connection.close();
+
+        // console.log(fruits);
+
+        fruits.forEach(function(fruit) {
+            console.log(fruit.name);
+        });
+    }
+})
